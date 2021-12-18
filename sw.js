24d40a679cd8faad3c73300dbb4a6b366fb2051a
qx (l14) <qx@juilliard.edu>
2021-12-18 15:12:23 -0500

    b0a52762c09765503b94f6d88615f87c0a01ad24

---
+++ sw.js
@@ -0,0 +1,95 @@
+var urlsToCache = [
+    '/style.css',
+    '/res/img/portrait.jpg',
+    '/res/img/cover.jpg'
+];
+
+
+
+urlsToCache.push("/cn/writing/2020-04-06_%E6%9C%AB%E6%97%A5%E8%88%B9%E7%A5%A8.html");
+urlsToCache.push("/cn/writing/2020-05-23_%E5%84%BF%E7%AB%A5%E8%8A%82%E7%89%B9%E8%BE%91.html");
+urlsToCache.push("/cn/writing/2020-05-24_%E5%B7%B4%E8%B5%AB%E7%9A%84G%E5%A4%A7%E8%B0%83%E5%B0%8F%E6%AD%A5%E8%88%9E%E6%9B%B2.html");
+urlsToCache.push("/cn/writing/2020-06-03-%E5%86%B0%E5%86%B7%E6%AD%A3%E8%83%BD%E9%87%8F.html");
+urlsToCache.push("/cn/writing/2021-05-11_%E6%9C%9D%E9%9F%B3%E8%AE%BF%E8%B0%88.html");
+urlsToCache.push("/404.html");
+urlsToCache.push("/cn/about/");
+urlsToCache.push("/cn/media/video/");
+urlsToCache.push("/cn/media/audio/");
+urlsToCache.push("/cn/media/image/");
+urlsToCache.push("/cn/media/");
+urlsToCache.push("/en/about/");
+urlsToCache.push("/en/media/video/");
+urlsToCache.push("/en/media/audio/");
+urlsToCache.push("/en/media/image/");
+urlsToCache.push("/en/media/");
+urlsToCache.push("/");
+urlsToCache.push("/cn/contact/");
+urlsToCache.push("/cn/logistics/");
+urlsToCache.push("/cn/writing/");
+urlsToCache.push("/en/contact/");
+urlsToCache.push("/en/logistics/");
+urlsToCache.push("/manifest.json");
+urlsToCache.push("/sw.js");
+urlsToCache.push("/cn/writing/%E4%B8%87%E6%9C%89%E6%80%A7%E8%B4%A8.html");
+urlsToCache.push("/cn/writing/%E4%B8%BA%E4%BB%80%E4%B9%88%E6%84%9F%E8%A7%89%E5%9C%A8%E9%95%BF%E5%A4%A7%E4%BB%A5%E5%90%8E%E7%96%B2%E5%8A%B3%E6%84%9F%E8%B6%8A%E6%9D%A5%E8%B6%8A%E9%9A%BE%E4%BB%A5%E9%80%9A%E8%BF%87%E7%9D%A1%E4%B8%80%E8%A7%89%E6%B6%88%E9%99%A4.html");
+urlsToCache.push("/cn/writing/%E4%BA%B2%E5%AF%86%E5%85%B3%E7%B3%BB%E4%B8%AD%E7%9A%84%E6%AD%A3%E5%8F%8D%E9%A6%88.html");
+urlsToCache.push("/cn/writing/%E5%A4%B1%E6%81%8B%E5%90%8E%E6%AF%8F%E5%A4%A9%E5%9D%9A%E6%8C%81%E5%81%A5%E8%BA%AB%E7%9C%8B%E4%B9%A6%E5%AD%A6%E4%B9%A0%E4%B8%BA%E4%BB%80%E4%B9%88%E4%BE%9D%E7%84%B6%E6%B2%A1%E6%9C%89%E5%BF%AB%E4%B9%90.html");
+urlsToCache.push("/cn/writing/%E5%AD%A6%E4%BC%9A%E4%B8%8E%E9%BB%91%E6%9A%97%E5%85%B1%E5%AD%98.html");
+urlsToCache.push("/cn/writing/%E6%96%87%E7%A7%91%E7%94%9F%E5%AD%A6%E7%94%B5%E8%84%91-git.html");
+urlsToCache.push("/cn/writing/%E6%96%87%E7%A7%91%E7%94%9F%E5%AD%A6%E7%94%B5%E8%84%91-git2.html");
+urlsToCache.push("/cn/writing/%E6%96%87%E7%A7%91%E7%94%9F%E5%AD%A6%E7%94%B5%E8%84%91-linux%E5%8A%9D%E8%BF%9B%E6%96%87.html");
+urlsToCache.push("/cn/writing/%E6%96%87%E7%A7%91%E7%94%9F%E5%AD%A6%E7%94%B5%E8%84%91-%E5%85%AC%E9%92%A5%E5%85%8D%E5%AF%86%E7%99%BB%E5%BD%95.html");
+urlsToCache.push("/cn/writing/%E6%96%87%E7%A7%91%E7%94%9F%E5%AD%A6%E7%94%B5%E8%84%91%E7%B3%BB%E5%88%97-%E6%96%87%E7%A7%91%E7%94%9F%E6%9B%B4%E5%8A%A0%E9%9C%80%E8%A6%81%E8%AE%A1%E7%AE%97%E6%9C%BA.html");
+urlsToCache.push("/cn/writing/%E6%96%87%E7%A7%91%E7%94%9F%E5%AD%A6%E7%94%B5%E8%84%91%E7%B3%BB%E5%88%97.html");
+urlsToCache.push("/cn/writing/%E7%90%86%E6%80%A7%E9%B8%A1%E6%B1%A4-%E6%83%85%E6%84%9F%E6%98%AF%E6%B6%88%E8%80%97%E5%93%81.html");
+urlsToCache.push("/cn/writing/%E7%90%86%E6%80%A7%E9%B8%A1%E6%B1%A4-%E6%94%BE%E5%BC%83%E4%BD%A0%E7%9A%84%E6%95%91%E4%B8%96%E4%B8%BB%E6%83%85%E7%BB%93.html");
+urlsToCache.push("/cn/writing/%E7%9F%A5%E4%B9%8E-%E4%B8%BA%E4%BB%80%E4%B9%88%E4%B9%90%E7%90%86%E8%B7%9F%E6%95%B0%E5%AD%A6%E5%A6%82%E6%AD%A4%E4%B8%8D%E5%90%8C.html");
+urlsToCache.push("/cn/writing/%E7%9F%A5%E4%B9%8E-%E6%95%B0%E5%AD%A6%E6%98%AF%E5%8F%91%E7%8E%B0%E8%BF%98%E6%98%AF%E5%8F%91%E6%98%8E.html");
+urlsToCache.push("/cn/writing/%E9%9A%94%E7%A6%BB%E7%BA%AA%E5%85%83-%E6%9C%AA%E6%9D%A5%E7%9A%84%E6%8F%90%E5%89%8D%E5%88%B0%E6%9D%A5.html");
+urlsToCache.push("/cn/writing/%E9%9F%B3%E4%B9%90-%E7%94%A8%E7%BE%A4%E8%AE%BA%E8%A7%A3%E9%87%8A%E4%BA%94%E5%BA%A6%E5%9C%88.html");
+urlsToCache.push("/cn/writing/%E9%BB%84%E9%87%91%E5%88%86%E5%89%B2.html");
+urlsToCache.push("/sitemap.xml");
+urlsToCache.push("/robots.txt");
+
+urlsToCache.filter((x) => x.slice(-5) == '.html').map((x) => urlsToCache.push(x.slice(0,-5)))
+
+var CACHE_NAME = 'offline-support';
+
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(function(cache) {
+    return urlsToCache.map((url) => cache.add(url).catch((err) => console.log(err)))
+  }));
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.match(event.request,{ignoreVary: true}).then(function (response) {
+        var req = fetch(event.request).then(function(response) {
+            if(!(new URL(event.request.url)).search){
+              console.log(`cache UPDATE for ${event.request.url}`)
+              cache.put(event.request, response.clone());
+            } 
+            return response;
+          });
+        if (response){
+            console.log(`cache HIT for ${event.request.url}`)
+            return response
+        }else{
+            console.log(`cache MISS for ${event.request.url}`)
+            return req
+        }   
+      });
+    })
+  );
+});
+
+self.addEventListener('message', function(event) {
+  if (event.data) { 
+    let data = event.data;
+    caches.open(CACHE_NAME).then(function (cache) {
+        cache.add(data)
+    })
+  }
+});
